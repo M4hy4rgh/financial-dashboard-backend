eec7c5a8da69a91b7bbabaf5db1b51f95e11e41f
M4hy4rgh <mahyarghasemi81@gmail.com>
2024-10-17 14:50:40 -0400

    Updated the controllers to enable CORS configuration.

--- financialdashboard/account-service/src/main/java/ca/avengers/accountservice/controller/AccountController.java
+++ financialdashboard/account-service/src/main/java/ca/avengers/accountservice/controller/AccountController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://financial-dashboard-seven-sigma.vercel.app/")
 @RestController
 @RequestMapping("/api/account")
 @RequiredArgsConstructor
