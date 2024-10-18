ce1c8c6da6619c7b2c501c69ce007fd4004be7b4
M4hy4rgh <mahyarghasemi81@gmail.com>
2024-10-18 02:30:59 -0400

    Resolved CORS configuration issue.

--- financialdashboard/transaction-service/src/main/java/ca/avengers/transactionservice/controller/TransactionController.java
+++ financialdashboard/transaction-service/src/main/java/ca/avengers/transactionservice/controller/TransactionController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
-@CrossOrigin(origins = {"https://financial-dashboard-seven-sigma.vercel.app", "http://localhost:5173"})
+@CrossOrigin(origins = {"https://financial-dashboard-seven-sigma.vercel.app"})
 @RestController
 @RequestMapping("/api/transaction")
 @RequiredArgsConstructor
